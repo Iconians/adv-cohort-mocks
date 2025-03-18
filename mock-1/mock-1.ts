c6876b002c7a63869638f4d3ebc77ac5a27914aa
Iconians <claytoncripe@gmail.com>
2025-03-17 17:32:22 -0700

    played with the first problems

--- mock-1/mock-1.ts
+++ mock-1/mock-1.ts
@@ -8,6 +8,27 @@
 // # Output: [1, 2]  (2 + 7 = 9)
 // # Input: numbers = [1, 3, 4, 5, 7], target = 8
 // # Output: [2, 4]
+const twoSum = (arr: number[], target: number) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    }
+    if (sum > target) {
+      right--;
+    }
+    if (sum < target) {
+      left++;
+    }
+  }
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [1, 2]
+console.log(twoSum([1, 3, 4, 5, 7], 8)); // [2, 4]
 
 // # Longest Substring Without Repeating Characters
 // # Given a string, find the length of the longest substring without repeating characters using the sliding window technique.
@@ -16,4 +37,3 @@
 // # Output: 3  ("abc")
 // # Input: "bbbbb"
 // # Output: 1  ("b")
-
